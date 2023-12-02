abcc58544649e0b74bdecea555a394e93f9e0118
menalin <menalin4@gmail.com>
2023-12-02 23:03:22 +0530

    changed code to removeadmin.js

--- dashboard/src/pages/RemoveAdmin.js
+++ dashboard/src/pages/RemoveAdmin.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+// RemoveAdmin.js
 
-const RemoveAdmin= () => {
+import React, { useState } from 'react';
+import '../components/remove.css';
+
+const RemoveAdmin = () => {
   const [adminData, setAdminData] = useState({
     adminName: '',
     password: '',
@@ -27,37 +30,36 @@ const RemoveAdmin= () => {
   };
 
   return (
-    <form align="center" style={{ paddingTop: "20px" }}>
-      Admin name:
-      <input type="text" name="fname" id="fname" value={adminData.adminName} /><br /><br />
-      Password:
-      <input type="text" name="lname" id="lname" value={adminData.password} /><br /><br />
-      Permissions:
-      <input type="text" name="age" id="age" value={adminData.permissions} /><br /><br />
-      <button onClick={handleRemoveAdmin}>Remove</button><br /><br />
-
-      <table id="table" border="1" align="center">
-        <thead>
-          <tr>
-            <th>Admin name</th>
-            <th>Password</th>
-            <th>Permissions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Map over your admin data array and display rows */}
-          {/* Set the handleRowClick function for each row */}
-          {/* You can replace this with your actual admin data array */}
-          {[1, 2, 3].map((item, index) => (
-            <tr key={index} onClick={() => handleRowClick(index)}>
-              <td>{/* Replace with your actual admin name */}</td>
-              <td>{/* Replace with your actual password */}</td>
-              <td>{/* Replace with your actual permissions */}</td>
+    <div className="remove-admin-container">
+      <table>
+          <thead>
+            <tr>
+              <th>Admin name</th>
+              <th>Password</th>
+              
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </form>
+          </thead>
+          <tbody>
+            {/* Sample row, replace with your actual admin data */}
+            <tr onClick={() => handleRowClick(0)}>
+              <td>John Doe</td>
+              <td>********</td>
+              
+            </tr>
+            {/* Add more rows as needed */}
+          </tbody>
+        </table><br></br>
+      <form className="form">
+        Admin name:
+        <input type="text" name="fname" id="fname" value={adminData.adminName} /><br /><br />
+        Password:
+        <input type="text" name="lname" id="lname" value={adminData.password} /><br /><br />
+        <div className="button">
+        <button type="submit" onClick={handleRemoveAdmin}>Remove</button><br /><br />
+        </div>
+        
+      </form>
+    </div>
   );
 };
 
