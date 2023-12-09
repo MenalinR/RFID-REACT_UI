12438b80592f26a4e101bdb3a5fa44de04d07b7f
menalin <menalin4@gmail.com>
2023-12-09 12:13:36 +0530

    added  searchbar in app.css files

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,46 +1,73 @@
 // App.js
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import { GiHamburgerMenu } from 'react-icons/gi';
+
 import Home from './pages/Home';
 import AddAdmin from './pages/AddAdmin';
-import RemoveAdmin from './pages/RemoveAdmin';
+
 import AddEmployee from './pages/AddEmployee';
 import ScheduleMeet from './pages/ScheduleMeet';
-import logo from './components/slt.png';
+import RemoveEmp from './pages/RemoveEmp';
+import Log from './pages/Log';
+
+
+import SLTMobitel from './components/sl.png';
+
 import './App.css';
+import './components/rmv.css';
 import './components/add.css';
 import './components/remove.css';
 import './components/ScheduleMeet.css';
 import './components/addemp.css';
 import './components/home.css';
-
-
+import './components/log.css';
 
 
 function App() {
-  const [showNav, setshowNav] = useState(false);
-  
+  const [showNav, ] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
 
   return (
     <Router>
       <header>
-        <GiHamburgerMenu onClick={() => setshowNav(!showNav)} />
-        <img src={logo} alt='Logo' className='logo'/>
+       <div className='m'>
+        <div className="image">
+        <img src={SLTMobitel} alt='Logo' className='logo'/>
+        <p>SMART  MEETING  ROOM  RESERVATION</p>
+        </div>
+        </div>
+        
+        
         
       </header>
+    
       <Navbar show={showNav} />
-      <div className="main">
-      <Routes>
-        <Route path="/Home" element={<Home />} />
+      <div className="main" >
+        <div className="searchbar">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="search-input"
+          />
+        </div>
+        
+        <Routes>
+        <Route path="/" element={<Navigate to="/Home" />} />
+      <Route path="/Home" element={<Home />} />
         <Route path="/AddAdmin" element={<AddAdmin />} />
-        <Route path="/RemoveAdmin" element={<RemoveAdmin />} />
+        
         <Route path="/AddEmployee" element={<AddEmployee />} />
         <Route path="/ScheduleMeet" element={<ScheduleMeet />} />
-      </Routes>
+        <Route path="/RemoveEmp" element={<RemoveEmp />} />
+        <Route path="/Log" element={<Log />} />
+
+        </Routes>
       </div>
     </Router>
+    
   );
 }
 
