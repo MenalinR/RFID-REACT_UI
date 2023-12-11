640c19a8cc4846037d17a9fb5df8a23d57ed615d
menalin <menalin4@gmail.com>
2023-12-11 14:43:37 +0530

    added more lables in Addemployee.js files

--- dashboard/src/pages/AddEmployee.js
+++ dashboard/src/pages/AddEmployee.js
@@ -1,5 +1,6 @@
 // AddEmployee.js
 import React from 'react';
+import '../components/addemp.css';
 
 const AddEmployee = () => {
   const handleSubmit = (e) => {
@@ -8,29 +9,44 @@ const AddEmployee = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} style={{ border: '1px solid #ccc' }}>
-        <div className="container">
-          <p>Create an employee</p>
-          <hr />
-
-          <label htmlFor="employeeName"><b>Employee Name</b></label>
-          <input type="text" placeholder="Employee Name" name="employeeName" required />
-
-          <label htmlFor="employeeID"><b>Employee ID</b></label>
+    <div className="page">
+      <form onSubmit={handleSubmit} className="form-container">
+        <div className="form-header">
+          <p>Create an Employee</p>
+        </div>
+        <div className="form-group">
+          <label>Employee Name</label>
+          <input type="text" placeholder="Enter Employee Name" name="employeeName" required />
+        </div>
+        <div className="form-group">
+          <label>Employee ID</label>
           <input type="text" placeholder="Enter ID" name="employeeID" required />
+        </div>
+        <div className="form-group">
+          <label>RFID No</label>
+          <input type="text" placeholder="Enter ID" name="employeeID" required />
+        </div>
+        <div className="form-group">
+  <label>Phone Number</label>
+  <input type="text" placeholder="Enter Phone Number" name="phoneNumber" required />
+</div><div className="form-group">
+  <label>Building</label>
+  <input type="text" placeholder="Enter Phone Number" name="phoneNumber" required />
+</div>
 
-          <label htmlFor="jobRole"><b>Job Role</b></label>
+
+        <div className="form-group">
+          <label>Job Role</label>
           <input type="text" placeholder="Enter Job Role" name="jobRole" required />
-
-          <label htmlFor="department"><b>Department</b></label>
+        </div>
+        <div className="form-group">
+          <label>Department</label>
           <input type="text" placeholder="Enter Department" name="department" required />
-
-          <div className="clearfix">
-            <button type="submit" className="signupbtn">
-              Add
-            </button>
-          </div>
+        </div>
+        <div className="form-group clearfix">
+          <button type="submit" className="signup-btn">
+            Add
+          </button>
         </div>
       </form>
     </div>
