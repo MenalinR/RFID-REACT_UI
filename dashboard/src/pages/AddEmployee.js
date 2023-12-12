0502c1b0264225be0ce4856fe14c5dac8c3848a6
menalin <menalin4@gmail.com>
2023-12-12 21:15:33 +0530

    added code view the employees details in  Addemployee.js file

--- dashboard/src/pages/AddEmployee.js
+++ dashboard/src/pages/AddEmployee.js
@@ -1,22 +1,34 @@
 // AddEmployee.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../components/addemp.css';
 
 const AddEmployee = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/')
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+  
     // Add your form submission logic here
   };
+
 
   return (
     <div className="page">
       <form onSubmit={handleSubmit} className="form-container">
-        <div className="form-header">
+      <div className="form-header">
           <p>Create an Employee</p>
         </div>
         <div className="form-group">
           <label>Employee Name</label>
-          <input type="text" placeholder="Enter Employee Name" name="employeeName" required />
+          <input type="text" placeholder="Enter Employee Name" name="employeeName" required  />
         </div>
         <div className="form-group">
           <label>Employee ID</label>
@@ -49,6 +61,41 @@ const AddEmployee = () => {
           </button>
         </div>
       </form>
+      <div className="table">
+      <h2>Employee Table</h2>
+      <table>
+        <thead>
+          <tr>
+            
+            <th>Name</th>
+            <th>ID</th>
+              <th>RFID No</th>
+            <th>Phone Number</th>
+            <th>Building</th>
+            <th>Job Role</th>
+            <th>Department</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((d) => (
+            <tr key={d.EID}>
+              <td>{d.name}</td>
+              <td>{d.EID}</td>
+              <td>{d.RFID_no}</td>
+              <td>{d.phone_no}</td>
+              <td>{d.department}</td>
+              <td>{d.building}</td>
+              <td>{d.role}</td>
+              <td>
+                
+                <button className='btn-primary'>Update</button>
+                <button className='btn-danger'>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        </table>
+    </div>
     </div>
   );
 };
