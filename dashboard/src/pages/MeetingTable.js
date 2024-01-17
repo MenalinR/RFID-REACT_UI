9beb10b26d4af48f85087b584585d5991af92f77
menalin <menalin4@gmail.com>
2024-01-17 14:41:40 +0530

    added code to filtered data int osearch bar

--- dashboard/src/pages/MeetingTable.js
+++ dashboard/src/pages/MeetingTable.js
@@ -42,9 +42,7 @@ const MeetingTable = () => {
     const searchTerm = event.target.value;
     setSearchTerm(searchTerm);
     const filtered = data.filter(d =>
-      Object.values(d).some(value =>
-        value.toString().toLowerCase().includes(searchTerm.toLowerCase())
-      )
+      d.topic.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setFilteredData(filtered);
   };
@@ -60,55 +58,52 @@ const MeetingTable = () => {
           onChange={handleSearch}
         />
 
-
-
       <div className="meeting-table-wrapper">
-        
         <table className="meeting-table">
-        <thead>
-          <tr>
-            <th>Reference Key</th>
-            <th>Topic</th>
-            <th>Participants</th>
-            <th>Meeting Date</th>
-            <th>Start Time</th>
-            <th>End Time</th>
-            <th>Organizer</th>
-            <th>Duration</th>
-            <th>Scheduled Date </th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((d) => (
-            <tr key={d.reference_key}>
-            <td>{d.reference_key}</td>
-              <td>{d.topic}</td>
-              <td>{d.Participants}</td>
-              <td>
-                {new Date(d.date).toLocaleDateString('en-GB', {
-                  day: 'numeric',
-                  month: 'numeric',
-                  year: 'numeric',
-                })}
-              </td>
-              <td>{d.start_time}</td>
-              <td>{d.end_time}</td>
-              <td>{d.scheduler}</td>
-              <td>{d.duration}</td>
-              <td>
-                {new Date(d.schldate).toLocaleDateString('en-GB', {
-                  day: 'numeric',
-                  month: 'numeric',
-                  year: 'numeric',
-                })}
-              </td>
-              <td>
-                <button onClick={() => handleDelete(d.reference_key)}>Delete</button>
-              </td>
+          <thead>
+            <tr>
+              <th>Reference Key</th>
+              <th>Topic</th>
+              <th>Participants</th>
+              <th>Meeting Date</th>
+              <th>Start Time</th>
+              <th>End Time</th>
+              <th>Organizer</th>
+              <th>Duration</th>
+              <th>Scheduled Date </th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
+          </thead>
+          <tbody>
+            {filteredData.map((d) => (
+              <tr key={d.reference_key}>
+                <td>{d.reference_key}</td>
+                <td>{d.topic}</td>
+                <td>{d.Participants}</td>
+                <td>
+                  {new Date(d.date).toLocaleDateString('en-GB', {
+                    day: 'numeric',
+                    month: 'numeric',
+                    year: 'numeric',
+                  })}
+                </td>
+                <td>{d.start_time}</td>
+                <td>{d.end_time}</td>
+                <td>{d.scheduler}</td>
+                <td>{d.duration}</td>
+                <td>
+                  {new Date(d.schldate).toLocaleDateString('en-GB', {
+                    day: 'numeric',
+                    month: 'numeric',
+                    year: 'numeric',
+                  })}
+                </td>
+                <td>
+                  <button onClick={() => handleDelete(d.reference_key)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
