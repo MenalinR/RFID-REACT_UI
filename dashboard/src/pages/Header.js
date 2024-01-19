2ad7f77d2e8a99e6ba53b0be102b76d6bd014f2d
menalin <menalin4@gmail.com>
2024-01-19 12:12:15 +0530

    changed the logo in header.js

--- dashboard/src/pages/Header.js
+++ dashboard/src/pages/Header.js
@@ -1,13 +1,13 @@
 // Header.js
 import React from 'react';
 import '../components/header.css';
-import SLTMobitel from '../components/sl.png';
+import mobi from '../components/mobi.png';
 
 const Header = () => {
   return (
     <div className="header">
        <div className="logo-container">
-          <img src={SLTMobitel} alt="Logo" className="logo" />
+          <img src={mobi} alt="Logo" className="logo" />
           <p>SMART MEETING ROOM RESERVATION</p>
         </div>
     </div>
