2995eb0d70351b02fe5f51d95ca6218a724a544c
menalin <menalin4@gmail.com>
2024-02-09 16:19:54 +0530

    added function to keeping the background color blue for the active page and added classname for all li contents in navbar.js

--- dashboard/src/pages/Navbar.js
+++ dashboard/src/pages/Navbar.js
@@ -1,36 +1,98 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { IoHome, IoPersonAdd } from 'react-icons/io5';
+import { Link, useLocation } from 'react-router-dom';
+import { IoHome } from 'react-icons/io5';
+import { IoPersonAdd } from 'react-icons/io5';
 import { AiOutlineSchedule } from 'react-icons/ai';
 import { MdPersonAddAlt } from 'react-icons/md';
 import { TbReportSearch } from 'react-icons/tb';
 import '../components/navbar.css';
+import { useState, useEffect } from 'react';
 
 const Navbar = ({ show, loggedInUser, userPermissions }) => {
-  const [selectedPage, setSelectedPage] = useState(null);
+  const permissions = userPermissions;
+  const location = useLocation();
+  const [activePage, setActivePage] = useState('');
 
-  const handlePageClick = (page) => {
-    setSelectedPage(page);
-  };
+  useEffect(() => {
+    // Update activePage when the location changes
+    setActivePage(location.pathname);
+  }, [location]);
 
-  const renderNavItem = (page, icon, label, permission) => (
-    <li className={selectedPage === page ? 'selected' : ''} key={page}>
-      <Link to={`/${page}`} onClick={() => handlePageClick(page)}>
-        {icon}
-        {label}
-      </Link>
-    </li>
-  );
+  const isPermissionGranted = (permission) => permissions && permissions.includes(permission);
 
   return (
     <div className={show ? 'sidenav active' : 'sidenav'}>
       <ul>
-        {renderNavItem('Home', <IoHome />, 'Home')}
-        {renderNavItem('AddAdmin', <IoPersonAdd />, 'Manage user', 'manage_user')}
-        {renderNavItem('ScheduleMeet', <AiOutlineSchedule />, 'Schedule Meeting', 'schedule_meeting')}
-        {renderNavItem('MeetingTable', <AiOutlineSchedule />, 'Meeting Details', 'meeting_details')}
-        {renderNavItem('AddEmployee', <MdPersonAddAlt />, 'Add Employee', 'user_creation')}
-        {renderNavItem('Log', <TbReportSearch />, 'Log', 'log')}
+        <li className={activePage === '/Home' ? 'selected' : ''}>
+          <Link to="/Home" onClick={() => setActivePage('/Home')}>
+            <IoHome />
+            <span className="menu-text">Home</span>
+          </Link>
+        </li>
+        <li className={activePage === '/AddAdmin' ? 'selected' : ''}>
+          {isPermissionGranted('manage_user') ? (
+            <Link to="/AddAdmin" onClick={() => setActivePage('/AddAdmin')}>
+              <IoPersonAdd />
+              <span className="menu-text"> Manage user</span>
+            </Link>
+          ) : (
+            <span>
+              <IoPersonAdd className="dull-color" />
+              <span className="menu-text dull-color">Manage user</span>
+            </span>
+          )}
+        </li>
+        <li className={activePage === '/ScheduleMeet' ? 'selected' : ''}>
+          {isPermissionGranted('schedule_meeting') ? (
+            <Link to="/ScheduleMeet" onClick={() => setActivePage('/ScheduleMeet')}>
+              <AiOutlineSchedule />
+              <span className="menu-text">Schedule Meeting</span>
+            </Link>
+          ) : (
+            <span>
+              <AiOutlineSchedule className="dull-color" />
+              <span className="menu-text dull-color"> Schedule Meeting</span>
+            </span>
+          )}
+        </li>
+        <li className={activePage === '/MeetingTable' ? 'selected' : ''}>
+          {isPermissionGranted('meeting_details') ? (
+            <Link to="/MeetingTable" onClick={() => setActivePage('/MeetingTable')}>
+              <AiOutlineSchedule />
+              <span className="menu-text">Meeting Details</span>
+            </Link>
+          ) : (
+            <span>
+              <AiOutlineSchedule className="dull-color" />
+             < span className="menu-text dull-color">  Meeting Details</span>
+            </span>
+          )}
+        </li>
+        <li className={activePage === '/AddEmployee' ? 'selected' : ''}>
+          {isPermissionGranted('user_creation') ? (
+            <Link to="/AddEmployee" onClick={() => setActivePage('/AddEmployee')}>
+              <MdPersonAddAlt />
+              <span className="menu-text">Add Employee</span> 
+            </Link>
+          ) : (
+            <span>
+              <MdPersonAddAlt className="dull-color" />
+              < span className="menu-text dull-color">Add Employee</span>
+            </span>
+          )}
+        </li>
+        <li className={activePage === '/Log' ? 'selected' : ''}>
+          {isPermissionGranted('log') ? (
+            <Link to="/Log" onClick={() => setActivePage('/Log')}>
+              <TbReportSearch />
+              <span className="menu-text">Log</span>
+            </Link>
+          ) : (
+            <span>
+              <TbReportSearch className="dull-color" />
+              <span className="menu-text dull-color"> Log</span>
+            </span>
+          )}
+        </li>
       </ul>
       <p id="pp">Powered by Digital Projects.</p>
     </div>
